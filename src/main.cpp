4cfb800b88f5b7778b17cc3ab2de352f6484b4ff
clovistessier <clovistessier@gmail.com>
2020-09-11 17:09:05 -0400

    Added 2nd mcp and OSC, sampling less buggy now.

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@
 #define CE1_PIN 17
 #endif
 
-uint8_t ss = 5;
 static const int clkSpd = 500000;
 SPIClass *vspi = NULL;
 
@@ -18,6 +17,18 @@ uint16_t reading;
 
 void vspiCommand(uint8_t *data, uint8_t size);
 uint16_t readADC(uint8_t channel);
+
+const char *ssid = "ESP32 Access Point";
+const char *password = "password";
+
+const IPAddress ip(192, 168, 1, 201);
+const IPAddress gateway(192, 168, 1, 1);
+const IPAddress subnet(255, 255, 255, 0);
+
+const char *host = "192.168.1.202";
+const int publish_port = 7000;
+const int recv_port = 7001;
+const char *args = "/trowacv/ch/1";
 
 void setup()
 {
@@ -27,30 +38,43 @@ void setup()
   vspi->begin();
 
   pinMode(CE0_PIN, OUTPUT);
+  pinMode(CE1_PIN, OUTPUT);
+  digitalWrite(CE1_PIN, HIGH);
   digitalWrite(CE0_PIN, HIGH);
+
+  WiFi.disconnect(true, true);
+  delay(2000);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+  WiFi.softAPsetHostname(ssid);
+  WiFi.softAPConfig(ip, gateway, subnet);
+
+  Serial.println("WiFi connected");
+  Serial.println("IP Address: ");
+  Serial.println(WiFi.softAPIP());
+
+  //OSC
+  OscWiFi.publish(host, publish_port, args, reading);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  // for (int i = 0; i < 8; i++)
-  // {
-  //   reading = readADC(i);
-  //   Serial.printf("%d\t", reading);
-  //   delay(10);
-  // }
-  // Serial.print("\n");
-  readADC(4);
-  vTaskDelay(30 / portTICK_PERIOD_MS);
-
+  for (int i = 0; i < 16; i++)
+  {
+    reading = readADC(i);
+    Serial.printf("%d\t", reading);
+  }
+  Serial.print("\n");
+  OscWiFi.update();
 }
 
-void vspiCommand(uint8_t *data, uint8_t size)
+void vspiCommand(uint8_t *data, const uint8_t size, const uint8_t ss)
 {
   vspi->beginTransaction(SPISettings(clkSpd, MSBFIRST, SPI_MODE0));
-  digitalWrite(5, LOW);
+  digitalWrite(ss, LOW);
   vspi->transfer(data, size);
-  digitalWrite(5, HIGH);
+  digitalWrite(ss, HIGH);
   vspi->endTransaction();
 }
 
@@ -60,22 +84,22 @@ uint16_t readADC(uint8_t channel)
   // first bit high is start bit
   // next bit indicates single-ended or differential reading
   // followed by ADC channel to be read
+  uint8_t ss = (channel >= 8) ? CE1_PIN : CE0_PIN;
 
-  uint8_t startBit = 1;
-  uint8_t singleBit = (1 << 7);
+  static uint8_t startBit = 1;
+  static uint8_t singleBit = (1 << 7);
   channel &= 0b00000111;
 
   uint8_t buffer[] = {startBit, (uint8_t)(singleBit | (channel << 4)), 0};
   // Serial.printf("buffer pre contents:  %d %d %d\n", buffer[0], buffer[1], buffer[2]);
 
   // Transfer the data to the ADC, the buffer is overwritten with the reading
-  vspiCommand(buffer, 3);
+  vspiCommand(buffer, 3, ss);
 
   // Serial.printf("buffer post contents: %d %d %d\n", buffer[0], buffer[1], buffer[2]);
 
   // ADC reading is located in the last 10 bits of the buffer
   uint8_t mask = 0b00000011;
   uint16_t result = ((buffer[1] & mask) << 8) | buffer[2];
-  Serial.println(result);
   return result;
 }
